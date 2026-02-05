7e1b196219dcb17a73edd5b9c32277e35d7413ae
John Andrés Arévalo Rodríguez <andresseconds@hotmail.com>
2026-02-05 17:58:33 -0500

    feat: crud de mesas funcionando

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { ProductsModule } from './products/products.module';
+import { TablesModule } from './tables/tables.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, TablesModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService] //Exportar para que otros módulos lo usen
---
+++ src/tables/tables.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { CreateTableDto } from './dto/create-table.dto';
+import { UpdateTableDto } from './dto/update-table.dto';
+
+@Injectable()
+export class TablesService {
+  constructor(private prisma: PrismaService){}
+
+  async create(createTableDto: CreateTableDto) {
+    return await this.prisma.table.create({
+      data: createTableDto
+    });
+  }
+
+  async findAll() {
+    return await this.prisma.table.findMany({
+      where: {isActive : true}
+    });
+  }
+
+  async findOne(id: number) {
+    const table = await this.prisma.table.findUnique({
+      where: {id}
+    });
+
+    if (!table) {
+      throw new NotFoundException(`Mesa con ID ${id} no encontrada`);
+    }
+    return table;
+  }
+
+  async update(id: number, updateTableDto: UpdateTableDto) {
+    // Primero verificamos si la mesa existe
+    await this.findOne(id);
+
+    return await this.prisma.table.update({
+      where : {id},
+      data: updateTableDto,
+    });
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} table`;
+  }
+}
